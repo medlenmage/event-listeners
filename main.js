494f13f5d8bdd6b428c15344991ba89b42f654c2
Joshua Medlen <joshua246@gmail.com>
2020-04-27 20:08:59 -0500

    pie filters added

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const pies = [
     crust: 'lattuce',
     price: 10,
     imgUrl: 'https://bit.ly/2xeAXnF',
-    owner: 'Matt',
+    owner: 'matt',
     aLaMode: true
   },
   {
@@ -23,35 +23,68 @@ const pies = [
     crust: 'standard',
     price: 3,
     imgUrl: 'https://images.food52.com/5qJW9R0kFGw0GtcV7gIi_MsqymQ=/1200x675/a38d55e1-e09b-4758-b301-449967da4fee--buttermilk_chess_pie.jpg',
-    owner: 'Michael',
+    owner: 'michael',
     aLaMode: true
   }
 ];
 
 
+// 1. make 4 buttons 1 for each instructor
+// 2 when you click on a button do the filter thing
+//   a. console log out a button click
+//   b. filter collection
+// 3. Reprint the dom with the filtered collection
+
+
+
 const printToDom = (selector, textToPrint) => {
   const selectedDiv = document.querySelector(selector);
-  selectedDiv.innerhtml = textToPrint;
+  selectedDiv.innerHTML = textToPrint;
 }
 
 const buildPies = (pieCollection) => {
-  let domString = ''
+  let domString = '';
 
   for (let i = 0; i < pieCollection.length; i++) {
-    domString += `<div class="pie">`
-    domString += `<h2>${pieCollection[i].type}</h2>`
-    domString += `<img src="${pieCollection[i].imgUrl}" alt="${pieCollection[i].type} pie">`
-    domString += `<p>Thid ${pieCollection[i].type} pie is a ${pieCollection[i].size} pie, it's owned ny ${pieCollection[i].owner} and has a crust of ${pieCollection[i].crust}</p>`
-    domString += `<h4>${pieCollection[i].price}</h4>`
-    domString += `</div>`
-    ;
+    domString += `
+      <div class="pie">
+        <h2>${pieCollection[i].type}</h2>
+        <img src="${pieCollection[i].imgUrl}" alt="image of ${pieCollection[i].type} pie">
+        <p>This ${pieCollection[i].type} pie is a ${pieCollection[i].size} pie, it's owned by ${pieCollection[i].owner}, and has a ${pieCollection[i].crust} crust.</p>
+        <h4>Price: ${pieCollection[i].price}</h4>
+      </div>
+    `;
   }
 
-  printToDom('#pieContainer', domString)
+  printToDom('#pieContainer', domString);
 }
+
+const filterPiesEvent= (event) => {
+  const buttonId = event.target.id;
+
+  const tempPieCollection = [];
+  
+  for (let i = 0; i < pies.length; i++) {
+    if (pies[i].owner === buttonId) {
+      tempPieCollection.push(pies[i]);
+    }
+    
+  }
+  buildPies(tempPieCollection);
+}
+
+
+const clickEvents = () => {
+  document.querySelector('#luke').addEventListener('click', filterPiesEvent);
+  document.querySelector('#michael').addEventListener('click', filterPiesEvent);
+  document.querySelector('#matt').addEventListener('click', filterPiesEvent);
+}
+
+
 
 const init = () => {
   buildPies(pies);
+  clickEvents();
 }
 
 init();
